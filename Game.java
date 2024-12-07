40290f2dbffcc7ea6ed6a60a72ca4b29682eb9ef
PutraHeryan <149023563+PutraHeryan@users.noreply.github.com>
2024-12-07 22:46:30 +0800

    Main Menu

---
+++ Game.java
@@ -0,0 +1,95 @@
+import java.awt.*;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import javax.swing.*;
+
+public class Game extends JPanel implements KeyListener {
+
+    private Player player;
+    private Obstacle obstacle;
+    private Scoreboard scoreboard;
+    private ThreadManager threadManager;
+    private boolean isGameRunning;
+    private Runnable onGameOver;
+
+    public Game(Runnable onGameOver) {
+        this.player = new Player(50, 300);
+        this.obstacle = new Obstacle(600, 300, 50, 50);
+        this.scoreboard = new Scoreboard();
+        this.threadManager = new ThreadManager(this);
+        this.isGameRunning = false;
+        this.onGameOver = onGameOver;
+
+        setPreferredSize(new Dimension(800, 600));
+        setBackground(Color.BLACK);
+        addKeyListener(this);
+        setFocusable(true);
+    }
+
+    public void startGame() {
+        isGameRunning = true;
+        threadManager.startGameLoop();
+    }
+
+    public void pauseGame() {
+        isGameRunning = false;
+        threadManager.stopGameLoop();
+    }
+
+    public void endGame() {
+        isGameRunning = false;
+        threadManager.stopGameLoop();
+        JOptionPane.showMessageDialog(this, "Game Over\nScore: " + scoreboard.getCurrentScore());
+        if (onGameOver != null) {
+            onGameOver.run();
+        }
+    }
+
+    public void resetGame() {
+        player.reset();
+        obstacle.resetPosition();
+        scoreboard.reset();
+        startGame();
+    }
+
+    @Override
+    public void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        if (isGameRunning) {
+            player.draw(g);
+            obstacle.draw(g);
+            scoreboard.display(g);
+        } else {
+            g.setColor(Color.WHITE);
+            g.drawString("Press Enter to Start", 350, 200);
+        }
+    }
+
+    public void update() {
+        if (isGameRunning) {
+            player.move();
+            obstacle.move();
+            if (player.checkCollision(obstacle)) {
+                endGame();
+            }
+            scoreboard.updateScore();
+            repaint();
+        }
+    }
+
+    @Override
+    public void keyTyped(KeyEvent e) {}
+
+    @Override
+    public void keyPressed(KeyEvent e) {
+        if (e.getKeyCode() == KeyEvent.VK_SPACE && isGameRunning) {
+            player.jump();
+        }
+        if (e.getKeyCode() == KeyEvent.VK_ENTER && !isGameRunning) {
+            resetGame();
+        }
+    }
+
+    @Override
+    public void keyReleased(KeyEvent e) {}
+}
