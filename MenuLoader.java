85fe1f8758564ee5501f66b1ebc2c38a448bcada
PutraHeryan <149023563+PutraHeryan@users.noreply.github.com>
2024-12-11 16:03:53 +0800

    Add files via upload
    Update 3

--- MenuLoader.java
+++ MenuLoader.java
@@ -8,7 +8,7 @@ public class MenuLoader {
 
     public static Map<String, String> loadMenuData(int idMenu) {
         Map<String, String> menuData = new HashMap<>();
-        String query = "SELECT Judul, Background, Start FROM Menu WHERE id_menu = ?";
+        String query = "SELECT Background, Start FROM Menu WHERE id_menu = ?";
 
         try (Connection connection = DatabaseConnector.getConnection();
              PreparedStatement statement = connection.prepareStatement(query)) {
@@ -17,7 +17,6 @@ public class MenuLoader {
             ResultSet resultSet = statement.executeQuery();
 
             if (resultSet.next()) {
-                menuData.put("Judul", resultSet.getString("Judul"));
                 menuData.put("Background", resultSet.getString("Background"));
                 menuData.put("Start", resultSet.getString("Start"));
             }
